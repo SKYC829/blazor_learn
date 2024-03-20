3466e2aa19b1d363451b89f5aa1e214ebea2226f
源峰 郑 <13677727680@163.com>
2024-03-20 15:59:29 +0800

    组件插槽

---
+++ BlazorLearn_WebApp/BlazorLearn_WebApp.Client/Components/L07/L07_Patient_Example/Models/HumanPatient.cs
@@ -0,0 +1,45 @@
+﻿
+namespace BlazorLearn_WebApp.Client.Components.L07.L07_Patient_Example.Models
+{
+    public class HumanPatient : IPatientBasicInfo
+    {
+        public string? Name { get; set; } = "姓名";
+        public int Age { get;set; }
+        public string? Category { get;set; }
+        public DateOnly? InhospitalDate { get;set; }
+        public TimeOnly? InhospitalTime { get;set; }
+
+        public IPatientAdditionalInfo GetAdditionInfo()
+        {
+            return new HumanAdditionInfo();
+        }
+    }
+
+    public class HumanAdditionInfo : IPatientAdditionalInfo
+    {
+        public string? Description { get; set; }
+    }
+
+    public class AnimalPatient : IPatientBasicInfo
+    {
+        public string? Name { get; set; } = "一只小鸟";
+        public int Age { get; set; } = 3;
+        public string? Category { get;set; }
+        public DateOnly? InhospitalDate { get;set; }
+        public TimeOnly? InhospitalTime { get;set; }
+
+        public IPatientAdditionalInfo GetAdditionInfo()
+        {
+            return new AnimalAdditionInfo()
+            {
+                IsEndangered = Random.Shared.NextDouble() >= 0.75
+            };
+        }
+    }
+    public class AnimalAdditionInfo:IPatientAdditionalInfo
+    {
+        public string? Description { get; set; }
+
+        public bool IsEndangered { get; set; }
+    }
+}
