c216f5c449b4ab48f05fea46f595dda118ea9477
源峰 郑 <13677727680@163.com>
2024-03-24 12:40:38 +0000

    Lesson 7 blazor render fragment

---
+++ BlazorLearn_WebApp/BlazorLearn_WebApp.Client/Components/L07/L07_Patient_Example/IPatientBasicInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorLearn_WebApp.Client.Components.L07.L07_Patient_Example
+{
+    public interface IPatientBasicInfo
+    {
+        string? Name { get; set; }
+
+        int Age { get; set; }
+
+        string? Category { get; set; }
+
+        DateOnly? InhospitalDate { get; set; }
+
+        TimeOnly? InhospitalTime { get; set; }
+
+        IPatientAdditionalInfo GetAdditionInfo();
+    }
+
+    public interface IPatientAdditionalInfo
+    {
+
+    }
+}
