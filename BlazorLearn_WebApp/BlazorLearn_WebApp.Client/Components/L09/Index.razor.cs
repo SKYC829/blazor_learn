daa22bf3c221eb6fefae52bb7f85379f2d899518
源峰 郑 <13677727680@163.com>
2024-04-01 09:56:43 +0800

    Blazor中的依赖注入

---
+++ BlazorLearn_WebApp/BlazorLearn_WebApp.Client/Components/L09/Index.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorLearn_WebApp.Client.Components.L09
+{
+    public partial class Index : ComponentBase
+    {
+        [Inject]
+        public SingletonService? SingletonService { get; set; }
+        [Inject]
+        public ScopedService? ScopedService { get; set; }
+        [Inject]
+        public TransientService? TransientService { get; set; }
+        [Inject]
+        public ILogger<Index>? Logger { get; set; }
+        public Task RaiseService()
+        {
+            return Task.Run(() =>
+            {
+                SingletonService?.SaySomething();
+                ScopedService?.SaySomething();
+                TransientService?.SaySomething();
+                Logger?.LogWarning($"--------{DateTime.Now}--------{"分割线"}--------");
+            });
+        }
+    }
+}
---
+++ BlazorLearn_WebApp/BlazorLearn_WebApp.Client/Components/L09/ScopedService.cs
@@ -0,0 +1,26 @@
+﻿
+namespace BlazorLearn_WebApp.Client.Components.L09
+{
+    public class ScopedService : ITestService
+    {
+        private readonly ILogger<ScopedService> _logger;
+        public Guid ServiceId => Guid.NewGuid();
+
+        public ScopedService(ILogger<ScopedService> logger)
+        {
+            _logger = logger;
+            _logger.LogInformation($"{nameof(ScopedService)} 服务 初始化");
+        }
+
+
+        public void Dispose()
+        {
+            _logger.LogInformation($"{nameof(ScopedService)} 服务 被释放");
+        }
+
+        public void SaySomething()
+        {
+            _logger.LogInformation($"当前 {nameof(ScopedService)} 的 {nameof(ServiceId)} 是 {ServiceId:N}");
+        }
+    }
+}
