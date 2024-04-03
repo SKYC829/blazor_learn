2d080bf15e9ea182cbfb3ecd756b6b1a545e09bf
源峰 郑 <13677727680@163.com>
2024-04-03 15:43:19 +0800

    Blazor的API交互

---
+++ BlazorLearn_WebApp/BlazorLearn_WebApp.Client/Components/L12/WeatherForecastApi.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorLearn_WebApp.Client.Components.L12
+{
+    public class WeatherForecastApi(IHttpClientFactory httpClientFactory,ILogger<WeatherForecastApi> logger)
+    {
+        public async Task<string> GetWeatherForecastAsync()
+        {
+            logger.LogInformation($"[包装器]:模拟干预请求之前的操作");
+            HttpClient localClient = httpClientFactory.CreateClient("local");
+            HttpResponseMessage response = await localClient.GetAsync("/WeatherForecast");
+            response.EnsureSuccessStatusCode();
+            logger.LogInformation($"[包装器]:模拟干预请求之后的操作");
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
--- BlazorLearn_WebApp/BlazorLearn_WebApp/Program.cs
+++ BlazorLearn_WebApp/BlazorLearn_WebApp/Program.cs
@@ -1,6 +1,7 @@
 using BlazorLearn_WebApp.Client;
 using BlazorLearn_WebApp.Client.Components.L02.TransferServiceExample;
 using BlazorLearn_WebApp.Client.Components.L09;
+using BlazorLearn_WebApp.Client.Components.L12;
 using BlazorLearn_WebApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,22 @@ builder.Services.AddScoped<TransferLogService> ();
 builder.Services.AddSingleton<SingletonService>()
     .AddScoped<ScopedService>()
     .AddTransient<TransientService>();
+
+builder.Services.AddTransient<MyHttpClientMiddleware>()
+    .AddTransient<YourHttpClientMiddleware>();
+
+builder.Services.AddSingleton<WeatherForecastApi>();
+
+builder.Services.AddHttpClient<HttpClient>("baidu",c =>
+{
+    c.BaseAddress = new Uri("https://www.baidu.com");
+});
+
+builder.Services.AddHttpClient("local", c =>
+{
+    c.BaseAddress = new Uri("https://localhost:7297");
+}).AddHttpMessageHandler<MyHttpClientMiddleware>()
+.AddHttpMessageHandler<YourHttpClientMiddleware>();
 
 var app = builder.Build();
 
